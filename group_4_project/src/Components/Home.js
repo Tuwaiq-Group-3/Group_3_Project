c9209a1b701c02a60cce510b550740e26b7a5256
sarahabbas10 <sarahabbas1418@gmail.com>
2021-11-08 16:46:17 +0300

    Home page done

--- group_4_project/src/Components/Home.js
+++ group_4_project/src/Components/Home.js
@@ -1,11 +1,9 @@
-import header from '../Img/header.jpg'
 import jewellery from '../Img/jewellery.jpeg'
 import women from '../Img/women blouses.jpg'
 import men from '../Img/men.jpeg'
 import electronic from '../Img/electronic.jpg'
 import menHeader from '../Img/menHeader2.png'
 import wHeader from '../Img/wHeader.jpg'
-import diamond from '../Img/diamond.jpg'
 import jHeader from '../Img/jHeader.jpg'
 import online from '../Img/online.jpg'
 import '../App.css';
@@ -20,16 +18,18 @@ return (
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4" class="active" aria-current="true"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item">
         <img class="d-block w-100 " src={online} width="130%" />
-       
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 " src={jHeader} width="130%" />
       </div>
       <div class="carousel-item active carousel-item-start">
         <img class="d-block w-100" src={menHeader}width="130%"/>
-      
       </div>
       <div class="carousel-item carousel-item-next carousel-item-start">
         <img class="d-block w-100" src={wHeader} width="80%"/>
